0a30067db3ffb2d885863ccbc020df7b6e9c4b09
user25 <user25@user25.ru>
2022-11-17 10:00:21 +0500

    Сделана первичная библиотека и добавлены тесты

---
+++ LongestWordLibraryClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestWordLibrary
+{
+    public class LongestWordLibraryClass
+    {
+        public static string FindLongestWord(string text)
+        {
+            if(text==string.Empty)
+            {
+                return string.Empty;
+            }
+            var text2 = text.Replace(",","").Replace(".","").Replace("!","").Replace("?","").Split(' ');
+            int maxlen = 0, index = 0;
+            for (int i = 0; i < text2.Length; i++)
+            {
+                if (text2[i].Length > maxlen)
+                {
+                    maxlen = text2[i].Length;
+                    index = i;
+                }
+            }
+           return  text2[index];
+        }
+    }
+}
---
+++ LongestWordLibraryTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LongestWordLibraryTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LongestWordLibraryTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6d7f2a0d-ac8b-4841-ae97-41322a2041f5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
